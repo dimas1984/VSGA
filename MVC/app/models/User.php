6d20a8ef19a670c717538e948844372a589d8155
dww <dimas.w@polinema.ac.id>
2024-07-25 15:14:27 +0700

    MVC

---
+++ MVC/app/models/User.php
@@ -0,0 +1,12 @@
+<?php
+class User {
+    public function getAllUsers() {
+        // Logika untuk mengambil semua pengguna dari database
+        return [
+            ['id' => 1, 'name' => 'dina'],
+            ['id' => 2, 'name' => 'dino'],
+            ['id' => 3, 'name' => 'dini']
+        ];
+    }
+}
+?>
---
+++ MVC/public/index.php
@@ -0,0 +1,9 @@
+<?php 
+require '../app/controllers/HomeController.php';
+require '../app/models/User.php';
+
+
+
+$controller = new HomeController();
+$controller->index();
+?>
